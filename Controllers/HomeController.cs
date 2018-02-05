c0912699b4bc9526d8697ef5475dc5eaae0c334b
Kayla Ondracek <kayla.renee@gmail.com>
2018-02-05 10:15:16 -0800

    add new routes including letter, make letter default root path

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/hello")]
+    public string Hello() { return "Hello friend!"; }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+    [Produces("text/html")]
+    [Route("/")]
+    public string Letter()
+    {
+      return
+      "<!DOCTYPE html>" +
+      "<html>" +
+      "<head>" +
+        "<title>Hello Friend!</title>" +
+        "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>" +
+      "</head>" +
+      "<body>" +
+        "<h1>Hello from afar</h1>" +
+        "<p>Dear friend,</p>" +
+        "<p>How are you? I hope that you are having a nice weekend. I'm learning programming while in Iceland! </p>" +
+       "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
+       "<p>But I like programming a lot, so I've got that going for me. </p>" +
+       "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
+       "<p>Cheers,</p>" +
+       "<p>Travel Enthusiast Jane</p>" +
+     "</body>" +
+     "</html>";
+
+    }
+  }
+}
