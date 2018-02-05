771907ee6b01c1d725e86a3da1708f034ab1c48d
Kayla Ondracek <kayla.renee@gmail.com>
2018-02-05 10:37:59 -0800

    add Views for letter and journal

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,29 +10,16 @@ namespace FriendLetter.Controllers
     [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend."; }
 
-    [Produces("text/html")]
     [Route("/")]
-    public string Letter()
+    public ActionResult Letter()
     {
-      return
-      "<!DOCTYPE html>" +
-      "<html>" +
-      "<head>" +
-        "<title>Hello Friend!</title>" +
-        "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>" +
-      "</head>" +
-      "<body>" +
-        "<h1>Hello from afar</h1>" +
-        "<p>Dear friend,</p>" +
-        "<p>How are you? I hope that you are having a nice weekend. I'm learning programming while in Iceland! </p>" +
-       "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-       "<p>But I like programming a lot, so I've got that going for me. </p>" +
-       "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-       "<p>Cheers,</p>" +
-       "<p>Travel Enthusiast Jane</p>" +
-     "</body>" +
-     "</html>";
+      return View();
+    }
 
+    [Route("/journal")]
+    public ActionResult Journal()
+    {
+      return View();
     }
   }
 }
